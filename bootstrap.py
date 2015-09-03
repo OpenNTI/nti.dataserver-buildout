82f5fb8e5ab4f793f9f378f47bb9ca4fbc1887c9
Carlos Sanchez <carlos.sanchez@nextthought.com>
2015-09-03 00:24:56 +0000

    bump zc.buildout; bootstrap first
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtPlatform/trunk/nti.dataserver-buildout@72256 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- bootstrap.py
+++ bootstrap.py
@@ -12,6 +12,7 @@
 #
 ##############################################################################
 """Bootstrap a buildout-based project
+
 Simply run this script in a directory containing a buildout.cfg.
 The script accepts buildout command-line options, so you can
 use the -c option to specify an alternate configuration file.
@@ -31,9 +32,12 @@ tmpeggs = tempfile.mkdtemp(prefix='bootstrap-')
 
 usage = '''\
 [DESIRED PYTHON FOR BUILDOUT] bootstrap.py [options]
+
 Bootstraps a buildout-based project.
+
 Simply run this script in a directory containing a buildout.cfg, using the
 Python that you want bin/buildout to use.
+
 Note that by using --find-links to point to local resources, you can keep
 this script from going over the network.
 '''
