3280480ce9a7ce0856ced4550e37f1679824cbc6
Carlos Sanchez <carlos.sanchez@nextthought.com>
2016-03-15 22:01:00 +0000

    bump setuptools;bump alpha; bootstrap first
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtPlatform/trunk/nti.dataserver-buildout@84717 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- ez_setup.py
+++ ez_setup.py
@@ -30,7 +30,7 @@ try:
 except ImportError:
     USER_SITE = None
 
-DEFAULT_VERSION = "20.2.2"
+DEFAULT_VERSION = "20.3"
 DEFAULT_URL = "https://pypi.python.org/packages/source/s/setuptools/"
 DEFAULT_SAVE_DIR = os.curdir
 
