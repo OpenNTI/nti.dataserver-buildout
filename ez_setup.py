bc3c59b09dcc7889cb18845930cf47814e65482b
Carlos Sanchez <carlos.sanchez@nextthought.com>
2016-07-21 13:19:28 +0000

    bump setuptools;webtest bootstrap first
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtPlatform/trunk/nti.dataserver-buildout@92790 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- ez_setup.py
+++ ez_setup.py
@@ -35,7 +35,7 @@ try:
 except ImportError:
     USER_SITE = None
     
-LATEST = "24.2.0"
+LATEST = "24.2.1"
 DEFAULT_VERSION = LATEST
 DEFAULT_URL_NT = "https://downloads.nextthought.com/deps/eggs/"
 DEFAULT_URL_PYPI = "https://pypi.io/packages/source/s/setuptools/"
