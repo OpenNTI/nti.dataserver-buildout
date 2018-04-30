e38a9e34080f1ac265f9c6d4f6b25e83ea6ac7c5
Carlos Sanchez <carlos.sanchez@nextthought.com>
2018-04-30 00:42:55 +0000

    upate version
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtPlatform/trunk/nti.dataserver-buildout@128624 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- ez_setup.py
+++ ez_setup.py
@@ -35,7 +35,7 @@ try:
 except ImportError:
     USER_SITE = None
     
-LATEST = "39.0.1"
+LATEST = "39.1.0"
 DEFAULT_VERSION = LATEST
 DEFAULT_URL_NT = "https://downloads.nextthought.com/deps/eggs/"
 DEFAULT_URL_PYPI = "https://pypi.io/packages/source/s/setuptools/"
