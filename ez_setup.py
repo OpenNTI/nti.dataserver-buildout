2c41f15b4b0977d41a1fe2d7e332680c7932673a
Josh Zuech <josh.zuech@nextthought.com>
2020-01-02 09:51:31 -0600

    Dep bmps

--- ez_setup.py
+++ ez_setup.py
@@ -35,7 +35,7 @@ try:
 except ImportError:
     USER_SITE = None
 
-LATEST = "42.0.2"
+LATEST = "44.0.0"
 DEFAULT_VERSION = LATEST
 DEFAULT_URL_NT = "https://downloads.nextthought.com/deps/eggs/"
 DEFAULT_URL_PYPI = "https://pypi.io/packages/source/s/setuptools/"
